988e30ff1ab6d59a810cb911dda8076dbbdad440
Siran Li <siranli411@gmail.com>
2024-06-07 16:24:13 -0500

    Calculator and tests

--- UIKitTable/Testing06062024/Calculator.swift
+++ UIKitTable/Testing06062024/Calculator.swift
@@ -11,4 +11,8 @@ struct Calculator {
     func sum(_ num1: Int, _ num2: Int) -> Int {
         num1 + num2
     }
+    
+    func multiply(_ num1: Int, _ num2: Int) -> Int {
+        num1 * num2
+    }
 }
