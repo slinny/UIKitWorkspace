7fc888d7b9580b905a0ca72e2a6f6e2526a0b923
Siran Li <siranli411@gmail.com>
2024-06-09 01:53:14 -0500

    Add right button on the navigationbar in Add CheckList

--- UIKitTable/HW06052024/ViewControllerAddCL.swift
+++ UIKitTable/HW06052024/ViewControllerAddCL.swift
@@ -5,16 +5,39 @@ class ViewControllerAddCL: UIViewController {
     @IBOutlet weak var itemNameTF: UITextField!
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var iconView: UIView!
+    var doneButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tapGestureSelectIcon()
+        addItemButton()
+    }
+    
+    fileprivate func addItemButton() {
+        doneButton = UIButton(type: .custom)
+        doneButton.setTitle("Done", for: .normal)
+        doneButton.setTitleColor(.systemGray, for: .normal)
+        doneButton.frame = CGRect(x: 300, y: 59, width: 47, height: 34)
+        let doneButtonBarItem = UIBarButtonItem(customView: doneButton)
+        navigationItem.rightBarButtonItem = doneButtonBarItem
+        
+        if let itemName = itemNameTF.text, itemName.count > 0, let iconImage = iconImageView.image {
+            doneButton.setTitleColor(.systemBlue, for: .normal)
+            doneButton.addTarget(self, action: #selector(doneButtonPressed(_:)), for: .touchUpInside)
+        }
     }
     
     fileprivate func tapGestureSelectIcon() {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(viewTapped(_:)))
         iconView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func doneButtonPressed(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let viewCAddI = storyboard.instantiateViewController(withIdentifier: "ViewControllerAddItem") as? ViewControllerAddItem {
+            navigationController?.pushViewController(viewCAddI, animated: true)
+        }
     }
     
     @objc func viewTapped(_ sender: UITapGestureRecognizer) {
