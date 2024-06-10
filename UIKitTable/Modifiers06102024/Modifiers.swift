e194048dccda55c82ff9900284a7fe8bff63244a
Siran Li <siranli411@gmail.com>
2024-06-10 18:59:54 -0500

    Workspace for Modifier

---
+++ UIKitTable/Modifiers06102024/Modifiers.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+/*
+ defualt modifier is internal
+ public accesible to outside module but not subclass or override
+ open accesible outside module
+ private only scope
+ fileprivate current file
+ internal
+ */
